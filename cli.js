94c15b5ac91d035838d37ee52bed26c4df38498c
Kossi D. T. Saka <kdtsaka@gmail.com>
2023-01-10 08:20:09 +0100

    WIP: Implement the method that greps translatable files and generates the localization templates

--- cli.js
+++ cli.js
@@ -40,6 +40,63 @@ async function create_l10n() {
     // Remove empty entries
     const files = cmdResults.split(/\r?\n/).filter(Boolean);
 
+    const l10nCollection = [];
+
+    let regexs = {
+        "__(*)": new RegExp(/\_\_\(\s?[\'|\"](.*?)[\'|\"]\s?\)/, "g"),
+        "_f(*, ...args)": new RegExp(/\_f\(\s?[\'|\"](.*?)[\'|\"]\s?,\s?.*?\)/, "g"),
+    };
+
+    /**
+     * Search for:
+     * - "__(*)"
+     *  e.g. __("search...text")     or  __('search...text')    or  __(`search...text`)
+     * - "_f(*, ...args)"
+     *  e.g. _f("search...text", *)  or  _f('search...text', *) or  _f(`search...text`, *)
+     */
+    let re = new RegExp(/\_[\_|f]\(\s?[\'|\`|\"]((?:.|\n)*?)[\'|\`|\"]\s?(?:\,\s?.*?)?\)/, "gm");
+
+    for (const file of files) {
+        let [fileContentErr, fileContent] = await resolver(fs.readFile(file, { encoding: "utf-8" }));
+        if (fileContentErr) {
+            console.error(fileContentErr);
+            continue;
+        }
+        let lines = fileContent
+            // split the filecontent into an array of lines
+            .split(/\r?\n/)
+            // transform each line into an object {lineNumber:x, line:""}
+            .map((line, idx, lines) => {
+                let lineObj = { lineNumber: idx + 1, line };
+                // recombine multilines translations
+                if (/\_[\_|f]\(\s?\`\s?/.test(line) && !/\s?\`\s?\)/.test(line)) {
+                    let nextLineIndex = idx + 1;
+                    lineObj.line += "\n" + lines[nextLineIndex];
+                    while (!/\s?\`\s?(?:\,\s?.*?)?\)/.test(lineObj.line) && nextLineIndex < lines.length) {
+                        lineObj.line += "\n" + lines[++nextLineIndex];
+                    }
+                }
+                return lineObj;
+            })
+            // remove unnecessary lines
+            .filter(({ line }) => /\_[\_|f]\(/.text(line));
+
+        // TODO: Verify the path
+        let relativeFilePath = path.relative(pathProjectRoot, file);
+
+        lines.forEach(entry => {
+            while ((match = re.exec(entry.line)) != null) {
+                l10nCollection.push({
+                    lineNumber: `${relativeFilePath}:${entry.lineNumber}`,
+                    text: match[1],
+                });
+            }
+        });
+    }
+
+    // TODO translform the l10nCollection into a pot template
+    // TODO translform the l10nCollection into a json template
+
     console.log(config);
     console.log(command);
     console.log(files);

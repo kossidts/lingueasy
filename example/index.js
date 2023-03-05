070675bb84450e2bd58a5f2785f85d9080d267ce
Kossi D. T. Saka <kdtsaka@gmail.com>
2023-03-05 23:26:35 +0100

    Add new endpoint for switching the site language as well as npm script to generate translations

--- example/index.js
+++ example/index.js
@@ -2,7 +2,7 @@ const path = require("node:path");
 const fs = require("node:fs");
 
 const express = require("express");
-
+const session = require("express-session");
 const lingueasy = require("lingueasy");
 
 const app = express();
@@ -10,8 +10,11 @@ const port = 8823;
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-
-lingueasy.init(app);
+// Session middleware have to be set before invoking lingueasy
+app.use(session({ secret: "very-radom-secret-key", resave: false, saveUninitialized: false, httpOnly: true }));
+lingueasy.init(app, {
+    // source_lang: "en"
+});
 
 app.get("/", (req, res) => {
     // The translating functions are available as
@@ -30,7 +33,29 @@ app.get("/", (req, res) => {
         title: "Translate With Lingueasy",
         content: _f("The server of this example page is listening on port %2$s.", 100, port),
     });
-    // Ein schnelles, offenes, unkompliziertes Web-Framework für Übersetzungen
+});
+
+app.get("/switch-language", (req, res) => {
+    const lingueasy = req.app.locals.lingueasy;
+    const target = req.query.redirectTo || "/";
+
+    /**
+     * Update the session to the new lang if:
+     * - query contains a lang (the query lang)
+     * - the query lang differs from the current lingueasy.lang (same as session.lang)
+     * - the query lang exists in the lingueasy.languages array, which contains all supported/available languages
+     */
+    if (req.query.lang && req.query.lang !== lingueasy.lang && lingueasy.languages.includes(req.query.lang)) {
+        req.session.lang = req.query.lang;
+        return req.session.save(err => {
+            if (err) {
+                console.error(err);
+            }
+            res.redirect(target);
+        });
+    }
+
+    res.redirect(target);
 });
 
 app.listen(port, () => {
